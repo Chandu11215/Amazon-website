34529912b442212d7c9eebba840d655cdb7d182a
KalalChandu <kalalshannu19@gmail.com>
2021-12-06 15:09:48 +0530

    amazon-clone

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello We're gona build Amazon website 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+
+    apiKey: "AIzaSyApYY_qlQ_mVHBs0mnPG9Pr8b2I7U-yeCQ",
+    authDomain: "shopping-cart-f871e.firebaseapp.com",
+    projectId: "shopping-cart-f871e",
+    storageBucket: "shopping-cart-f871e.appspot.com",
+    messagingSenderId: "876081928140",
+    appId: "1:876081928140:web:edc085b873703aa1a1af69",
+    measurementId: "G-42RQ0HFFRJ"
+};
