c6ef4b6775f55d551094bebe9d099ce784baf37e
KalalChandu <kalalshannu19@gmail.com>
2021-12-06 15:56:03 +0530

    modifying Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello We're gona build Amazon website 🚀</h1>
+      <Header />
     </div>
   );
 }
