45e0659abf9cc3b046361aa8bb5a7dea177e032c
KalalChandu <kalalshannu19@gmail.com>
2021-12-06 16:34:36 +0530

    css files are added

--- src/Header.js
+++ src/Header.js
@@ -1,49 +1,49 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
     return (
         <div className="header">
             <img 
-                className="header_logo"
-                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" 
-                alt="Amazon-img"
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                alt="amazon"
             />
-            
 
-            <div className="header_search">
+            <div className="header__search">
                 <input
-                    className="header_searchIn"
+                    className="header__searchInput"
                     type="text" />
                     {/* Logo */ }
             </div>
 
-            <div className="header_nav">
-                <div className="header_option">
-                    <span className='header_optionLineOne'>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className='header__optionLineOne'>
                         Hello Guest
                     </span>
 
-                    <span className='header_optionLineTwo'>
+                    <span className='header__optionLineTwo'>
                         Sign In
                     </span>
                 </div>
 
-                <div className="header_option">
-                    <span className='header_optionLineOne'>
+                <div className="header__option">
+                    <span className='header__optionLineOne'>
                         Returns
                     </span>
 
-                    <span className='header_optionLineTwo'>
+                    <span className='header__optionLineTwo'>
                         Orders
                     </span>
                 </div>
 
-                <div className="header_option">
-                    <span className='header_optionLineOne'>
+                <div className="header__option">
+                    <span className='header__optionLineOne'>
                         Your
                     </span>
 
-                    <span className='header_optionLineTwo'>
+                    <span className='header__optionLineTwo'>
                         Prime
                     </span>
                 </div>
